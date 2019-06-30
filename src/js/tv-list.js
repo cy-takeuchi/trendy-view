832306d53c8307eec85d09c950e19f4e42b312d4
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-06-30 15:19:35 +0900

    lightSlider対応

--- src/js/tv-list.js
+++ src/js/tv-list.js
@@ -2,79 +2,36 @@
     'use strict';
 
     const pluginConfig = window.tv.pluginConfig;
-    console.log(pluginConfig);
 
     let getRecords = async (query) => {
         let res = await window.tv.kintoneRecord.getRecords(window.tv.appId, query);
         return res;
     }
 
-    let createColumnList = (record) => {
-        let columnList = {};
-        let i = 0;
-        for (let fieldCode of Object.keys(record)) {
-            columnList[`column${i}`] = fieldCode;
-            i++;
-        }
-
-        return columnList;
-    }
-
-    let createValueNames = (columnList) => {
-        return Object.keys(columnList);
-    }
-
-    let createListHeader = (columnList) => {
-        let item = '<tr>';
-        for (let key of Object.keys(columnList)) {
-            item += `<td><div class="${key}"></div></td>`;
-        }
-        item += '</tr>';
-
-        return item;
-    }
-
-    let createItemList = (columnList, records) => {
-        let result = [];
-        for (let record of records) {
-            let item = {};
-            console.log(record);
-            for (let key of Object.keys(columnList)) {
-                let fieldCode = columnList[key];
-                item[key] = record[fieldCode].value;
-            }
-            result.push(item);
-        }
-
-        return result;
-    }
-
     kintone.events.on(['mobile.app.record.index.show'], (event) => {
-        if (event.viewId !== 5698734) {
+        if (event.viewId !== 5699240) {
             return event;
         }
 
         getRecords('').then((res) => {
-            let records = res.records;
+            const records = res.records;
 
-            let columnList = [];
             for (let i = 0; i < records.length; i++) {
-                let record = records[i];
-                if (i === 0) {
-                    columnList = createColumnList(record);
-                }
-
+                const record = records[i];
+                const title = record['案件名'].value;
+                const date = record['受注予定日'].value;
+                let html = '';
+                html += '<li>';
+                html += `<p>${title}</p>`;
+                html += `<p>${date}</p>`;
+                html += '</li>';
+                $('ul#tv-light-slider').append(html);
             }
 
-            let options = {
-                valueNames: createValueNames(columnList),
-                item: createListHeader(columnList)
-            };
-
-            let itemList = createItemList(columnList, records);
-
-            let list = new List('tv-list', options, itemList);
+            $("ul#tv-light-slider").lightSlider({
+                item: 1
+            });
         });
     });
 
-})();
+})(jQuery);
