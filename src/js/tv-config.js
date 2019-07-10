c156ce62a436c3d8ce4159107a44b8f31e1b954a
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-07-11 02:16:09 +0900

    プラグイン設定画面のテーブル対応

--- src/js/tv-config.js
+++ src/js/tv-config.js
@@ -11,23 +11,38 @@ jQuery.noConflict();
   };
 
   const getFormFieldList = async () => {
-    const res = await KintoneConfigHelper.getFields();
-    return res.filter((formField) => formField.type.includes(coveredFieldTypeList));
+    const res = await KintoneConfigHelper.getFields(...coveredFieldTypeList);
+    const fieldList = res.map((field) => {
+      return {label: `${field.label} (${field.code})`, value: field.code};
+    });
+    return fieldList;
+  };
+
+  const getTableInitialData = (name, fieldList) => {
+    return [
+      {
+        [name]: {
+          items: fieldList
+        }
+      }
+    ];
+  };
+
+  const getTableDefaultRowData = (name, fieldList) => {
+    return {
+      [name]: {
+        items: fieldList
+      }
+    };
   };
 
   const kUiSaveButton = new kintoneUIComponent.Button({
     text: 'Save',
     type: 'submit'
   });
-  $('div#tv-save-button').append(kUiSaveButton.render());
-
-  getFormFieldList().then((res) => {
-    console.log('res', res);
-  });
 
   kUiSaveButton.on('click', (e) => {
     e.preventDefault();
-
     let newPluginConfig = {};
 
     kintone.plugin.app.setConfig(newPluginConfig, () => {
@@ -35,4 +50,21 @@ jQuery.noConflict();
       window.location.href = getSettingsUrl();
     });
   });
+
+  getFormFieldList().then((fieldList) => {
+    const kUiTable = new kintoneUIComponent.Table({
+      data: getTableInitialData('fields', fieldList),
+      defaultRowData: getTableDefaultRowData('fields', fieldList),
+      columns: [
+        {
+          header: 'Field name',
+          cell: () => { return kintoneUIComponent.createTableCell('dropdown', 'fields') }
+        }
+      ]
+    });
+    $('div#tv-field-table').append(kUiTable.render());
+
+    $('div#tv-save-button').append(kUiSaveButton.render());
+  });
+
 })(jQuery);
