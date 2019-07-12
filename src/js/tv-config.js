2474116484912c5fd3d2e8cac833c2424af6c5a1
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-07-12 09:18:37 +0900

    プラグイン設定後にフィールドを追加した場合の対応

--- src/js/tv-config.js
+++ src/js/tv-config.js
@@ -58,6 +58,12 @@ jQuery.noConflict();
 
     $('div#tv-save-button').append(kUiSaveButton.render());
 
+
+
+    for (const showField of originalPluginConfig.showFieldList) {
+      // フィールドが増加した場合に、増加した分も含んだドロップダウンにする
+      showField.fields.items = fieldList;
+    }
     kUiTable.setValue(originalPluginConfig.showFieldList);
 
     kUiSaveButton.on('click', (e) => {
