853ca84b82fba662eba27eb3ee981af8a65d67b7
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2019-07-12 10:47:35 +0900

    プラグイン設定後にフィールドを削除した場合の対応

--- src/js/tv-config.js
+++ src/js/tv-config.js
@@ -58,11 +58,16 @@ jQuery.noConflict();
 
     $('div#tv-save-button').append(kUiSaveButton.render());
 
-
-
     for (const showField of originalPluginConfig.showFieldList) {
       // フィールドが増加した場合に、増加した分も含んだドロップダウンにする
       showField.fields.items = fieldList;
+
+      // フィールドが減少した場合に、減少した分を選択していた場合は未選択にする
+      const selectedValue = showField.fields.value;
+      const containsDropdown = showField.fields.items.find(item => item.value === selectedValue);
+      if (containsDropdown === undefined) {
+        showField.fields.value = undefined;
+      }
     }
     kUiTable.setValue(originalPluginConfig.showFieldList);
 
